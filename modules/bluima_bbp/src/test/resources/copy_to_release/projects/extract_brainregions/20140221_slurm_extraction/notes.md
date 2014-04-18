fea01d9ef8fd0a4fc35779f35b525644581462b3
renaud <renaud@apache.org>
2014-04-18 02:06:34 +0200

    print out species

--- modules/bluima_bbp/src/test/resources/copy_to_release/projects/extract_brainregions/20140221_slurm_extraction/notes.md
+++ modules/bluima_bbp/src/test/resources/copy_to_release/projects/extract_brainregions/20140221_slurm_extraction/notes.md
@@ -5,11 +5,12 @@ large scale extraction to build BR-connectivity knowledge base
 * NERs
     * BAMS
     * ABA
+    * neuroNER
 * then, for each: find Cooccurrences with
     * KERNEL
     * TOPDOWN
     * RULES
 * output:
     * dir: /nfs4/bbp.epfl.ch/simulation/nlp/data/20140221_br_rels_XXX
-    * naming[bams|aba][batchid]_[rels|entities].tsv
+    * naming[bams|aba|neuroner][batchid]_[rels|entities].tsv
     * see aggregates there
--- modules/bluima_commons/src/test/java/ch/epfl/bbp/uima/ae/LinnaeusAnnotatorTest.java
+++ modules/bluima_commons/src/test/java/ch/epfl/bbp/uima/ae/LinnaeusAnnotatorTest.java
@@ -1,6 +1,7 @@
 package ch.epfl.bbp.uima.ae;
 
 import static ch.epfl.bbp.ResourceHelper.getFile;
+import static ch.epfl.bbp.uima.testutils.UimaTests.assertResultsContains;
 import static ch.epfl.bbp.uima.testutils.UimaTests.getTestCas;
 import static org.apache.commons.io.FileUtils.readFileToString;
 import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngine;
@@ -8,7 +9,6 @@ import static org.apache.uima.fit.util.JCasUtil.select;
 
 import java.util.Collection;
 
-import org.apache.uima.analysis_engine.AnalysisEngine;
 import org.apache.uima.jcas.JCas;
 import org.junit.Test;
 
@@ -18,38 +18,34 @@ import ch.epfl.bbp.uima.typesystem.Prin;
 
 public class LinnaeusAnnotatorTest {
 
-	@Test
-	public void test() throws Exception {
+    @Test
+    public void test() throws Exception {
 
-		String testtext = readFileToString(getFile("linnaeus/pmcA1239921.txt"));
-		JCas jCas = getTestCas(testtext);
+        String testtext = readFileToString(getFile("linnaeus/pmcA1239921.txt"));
+        JCas jCas = getTestCas(testtext);
 
-		AnalysisEngine ae = createEngine(LinnaeusAnnotator.class);
-		ae.process(jCas);
+        createEngine(LinnaeusAnnotator.class).process(jCas);
 
-		Collection<LinnaeusSpecies> sps = select(jCas, LinnaeusSpecies.class);
-		Prin.t(sps);
+        Collection<LinnaeusSpecies> sps = select(jCas, LinnaeusSpecies.class);
+        Prin.t(sps);
 
-		UimaTests.assertResultsContains(sps, "mostProbableSpeciesId",
-		        "species:ncbi:9606");
-		UimaTests.assertResultsContains(sps, "mostProbableSpeciesId",
-		        "species:ncbi:9913");
-	}
+        assertResultsContains(sps, "mostProbableSpeciesId", "species:ncbi:9606");
+        assertResultsContains(sps, "mostProbableSpeciesId", "species:ncbi:9913");
+    }
 
-	@Test
-	public void test2() throws Exception {
+    @Test
+    public void test2() throws Exception {
 
-		// TODO won't work
-		// JCas jcas = UimaTests.getTestCas("bla bla rodent bla bla");
-		JCas jcas = getTestCas("bla bla rat bla bla");
+        // TODO won't work
+        // JCas jcas = UimaTests.getTestCas("bla bla rodent bla bla");
+        JCas jCas = getTestCas("bla bla rat bla bla");
 
-		AnalysisEngine ae = createEngine(LinnaeusAnnotator.class);
-		ae.process(jcas);
+        createEngine(LinnaeusAnnotator.class).process(jCas);
 
-		Collection<LinnaeusSpecies> sps = select(jcas, LinnaeusSpecies.class);
-		Prin.t(sps);
+        Collection<LinnaeusSpecies> sps = select(jCas, LinnaeusSpecies.class);
+        Prin.t(sps);
 
-		UimaTests.assertResultsContains(sps, "mostProbableSpeciesId",
-		        "species:ncbi:10116");
-	}
+        UimaTests.assertResultsContains(sps, "mostProbableSpeciesId",
+                "species:ncbi:10116");
+    }
 }
