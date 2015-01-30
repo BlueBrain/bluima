ba344468eb54c446f36cb5dda9f6ade02e82db2b
renaud <renaud@apache.org>
2015-01-30 10:20:45 +0100

    more tests on TrieId

--- utils/blue_commons/src/main/java/ch/epfl/bbp/triechar/TrieId.java
+++ utils/blue_commons/src/main/java/ch/epfl/bbp/triechar/TrieId.java
@@ -1,8 +1,13 @@
 package ch.epfl.bbp.triechar;
 
+import static java.lang.Integer.parseInt;
+
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.Serializable;
+
+import ch.epfl.bbp.io.LineReader;
 
 /**
  * Trie implementation to store strings (multiple occurences allowed) and
@@ -25,6 +30,7 @@ public class TrieId implements Serializable {
     }
 
     public int addWord(String word) {
+        if (word==null||word.length()==0)throw new IllegalArgumentException("word cannot be null or empty");;
         Integer id = trie.getWordData(word);
         if (id == null) {
             id = maxId;
@@ -50,5 +56,31 @@ public class TrieId implements Serializable {
 
     public void toIdsFile(OutputStream os) throws IOException {
         trie.toIdFile(os);
+    }
+
+    @SuppressWarnings("resource")
+    public static TrieId fromIdsFile(InputStream is) throws IOException {
+        TrieId t = new TrieId();
+
+        for (String line : new LineReader(is)) {
+            String[] split = line.split("\t");
+            if (split.length < 2) {
+                throw new IllegalArgumentException(
+                        "line '"
+                                + line
+                                + "' does not contain a tab. Format must be 'word{tab}id'");
+            } else if (split.length == 2) {
+                t.trie.addWord(split[0], parseInt(split[1]));
+            } else  {
+                StringBuilder sb = new StringBuilder();
+                sb.append(split[0]);
+                for (int i = 1; i < split.length - 1; i++) {
+                    sb.append('\t');
+                    sb.append(split[i]);
+                }
+                t.trie.addWord(sb.toString(), parseInt(split[split.length - 1]));
+            }
+        }
+        return t;
     }
 }
--- utils/blue_commons/src/test/java/ch/epfl/bbp/triechar/TrieIdTest.java
+++ utils/blue_commons/src/test/java/ch/epfl/bbp/triechar/TrieIdTest.java
@@ -5,6 +5,7 @@ import static java.lang.System.currentTimeMillis;
 import static org.junit.Assert.assertEquals;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
 import org.junit.Test;
@@ -38,19 +39,53 @@ public class TrieIdTest {
         assertEquals(0, t.getWordId("he"));
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void testEmptyString() {
+        TrieId t = new TrieId();
+        t.addWord("");
+    }
+
     @Test
     public void testWrite() throws Exception {
 
         TrieId t = new TrieId();
-        t.addWord("four");
+        t.addWord("zero");
+        t.addWord("one");
         t.addWord("two");
         t.addWord("three");
-        t.addWord("one");
-        t.addWord("zero");
+        t.addWord("four");
 
         String file = "target/trieIdTest_" + currentTimeMillis() + ".txt";
         t.toIdsFile(new FileOutputStream(file));
-        assertEquals("four\t0\ntwo\t1\nthree\t2\none\t3\nzero\t4\n",
+        assertEquals("four\t4\ntwo\t2\nthree\t3\none\t1\nzero\t0\n",
                 asText(new File(file)));
+
+        TrieId tSer = TrieId.fromIdsFile(new FileInputStream(file));
+        assertEquals(0, tSer.getWordId("zero"));
+        assertEquals(4, tSer.getWordId("four"));
+        assertEquals(-1, tSer.getWordId("blah"));
+    }
+
+    @Test
+    public void testSerDeser() throws Exception {
+
+        for (String testString : new String[] { "simple", "a\tb", "a\tb\t",
+                "a\t\tb", "\t" }) {
+
+            TrieId t = new TrieId();
+            t.addWord("0");
+            t.addWord(testString);
+            t.addWord("2");
+
+            String file = "target/trieIdTest_" + currentTimeMillis() + ".txt";
+            t.toIdsFile(new FileOutputStream(file));
+
+            TrieId tSer = TrieId.fromIdsFile(new FileInputStream(file));
+            assertEquals(0, tSer.getWordId("0"));
+            assertEquals("could not find '" + testString + "' back in trie", 1,
+                    tSer.getWordId(testString));
+            assertEquals(2, tSer.getWordId("2"));
+            assertEquals(-1, tSer.getWordId("blah"));
+        }
     }
 }
