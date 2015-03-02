accf26e618b6f4507823ad842903e9f468aa0183
Marco Antognini <antognini.marco@gmail.com>
2015-03-02 10:52:31 +0100

    merge sentence splitter tests

--- modules/bluima_opennlp/src/test/java/ch/epfl/bbp/uima/julielab/opennlp/OpenNlpHelperTest.java
+++ modules/bluima_opennlp/src/test/java/ch/epfl/bbp/uima/julielab/opennlp/OpenNlpHelperTest.java
@@ -1,15 +1,18 @@
 package ch.epfl.bbp.uima.julielab.opennlp;
 
+import static org.apache.uima.fit.pipeline.SimplePipeline.runPipeline;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Collection;
 import java.util.Iterator;
 
+import org.apache.uima.UIMAException;
 import org.apache.uima.fit.pipeline.SimplePipeline;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 import org.junit.Test;
 
+import ch.epfl.bbp.shaded.opennlp.tools.util.Pair;
 import ch.epfl.bbp.uima.ae.OpenNlpHelper;
 import ch.epfl.bbp.uima.testutils.UimaTests;
 import ch.epfl.bbp.uima.typesystem.Prin;
@@ -34,20 +37,41 @@ public class OpenNlpHelperTest {
     }
 
     @Test
-    public void testSSplitter() throws Exception {
+    public void testSentenceSplitter() throws UIMAException {
+        String text = "First sentence. Second sentence!";
+        Pair offsets[] = { new Pair(0, 15), new Pair(16, 32) };
 
-        JCas jCas = UimaTests
-                .getTestCas("Terminologies which lack semantic connectivity hamper the effective search in biomedical fact databases and document retrieval systems. We here focus on the integration of two such isolated resources, the term lists from the protein fact database UNIPROT and the indexing vocabulary MESH from the bibliographic database MEDLINE.");
-        SimplePipeline.runPipeline(jCas, OpenNlpHelper.getSentenceSplitter());
+        // Run the splitter annotator on `text`
+        JCas jCas = UimaTests.getTestCas(text);
+        runPipeline(jCas, OpenNlpHelper.getSentenceSplitter());
 
-        Collection<Sentence> st = JCasUtil.select(jCas, Sentence.class);
-        assertEquals(2, st.size());
+        Collection<Sentence> collection = JCasUtil.select(jCas, Sentence.class);
+        Sentence sentences[] = new Sentence[collection.size()];
+        collection.toArray(sentences);
 
-        Iterator<Sentence> it = st.iterator();
-        it.hasNext();
-        assertEquals(
-                "Terminologies which lack semantic connectivity hamper the effective search in biomedical fact databases and document retrieval systems.",
-                it.next().getCoveredText());
+        // Make sure the number of sentences and their offsets are corrects
+        assertEquals(sentences.length, offsets.length);
+        for (int i = 0; i < offsets.length; ++i) {
+            Sentence sentence = sentences[i];
+            Pair offset = offsets[i];
+
+            assertEquals(sentence.getBegin(), offset.a);
+            assertEquals(sentence.getEnd(), offset.b);
+        }
+
+        // Test it on more complex sentences
+        String sentence1 = "Terminologies which lack semantic connectivity hamper the effective search in biomedical fact databases and document retrieval systems.";
+        String sentence2 = "We here focus on the integration of two such isolated resources, the term lists from the protein fact database UNIPROT and the indexing vocabulary MESH from the bibliographic database MEDLINE.";
+        text = String.format("%s %s", sentence1, sentence2);
+        jCas = UimaTests.getTestCas(text);
+        runPipeline(jCas, OpenNlpHelper.getSentenceSplitter());
+
+        collection = JCasUtil.select(jCas, Sentence.class);
+        assertEquals(2, collection.size());
+
+        Iterator<Sentence> it = collection.iterator();
+        assertEquals(it.next().getCoveredText(), sentence1);
+        assertEquals(it.next().getCoveredText(), sentence2);
     }
 
     @Test
