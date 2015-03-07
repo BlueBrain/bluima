79eb6a57e2ae1486e39a99f8be895a9568493097
Marco Antognini <antognini.marco@gmail.com>
2015-03-07 15:40:39 +0100

    add TODO (nice to have but not vital)

--- install_resources.sh
+++ install_resources.sh
@@ -22,6 +22,16 @@ done
 
 # Additionally, test the installed models
 
+# TODO update script to automatically create tests for
+# any class that implement ModelResource interface.
+#  * grep "implement ModelResouce"
+#  * get file name (and assume it's also the class name)
+#  * find the pom.xml corresponding to that resource
+#    (simply go up in file hierarchy) and extract info
+#  * add dependency in bluima_dummymodelproxy
+#  * add test class based on stubs in
+#    tests/bluima_dummymodelproxy/src/test/java/ch/epfl/bbp/nlp
+
 pushd "tests/bluima_dummymodelproxy" > /dev/null
 
 mvn --quiet test
