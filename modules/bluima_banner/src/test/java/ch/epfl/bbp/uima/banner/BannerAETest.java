8ddb428c5e469218af62f7751c6b082a94f7d9f2
renaud <renaud@apache.org>
2015-06-09 19:59:09 +0200

    div

--- modules/bluima_banner/src/test/java/ch/epfl/bbp/uima/banner/BannerAETest.java
+++ modules/bluima_banner/src/test/java/ch/epfl/bbp/uima/banner/BannerAETest.java
@@ -4,6 +4,8 @@ import static ch.epfl.bbp.uima.BlueUima.PARAM_VIEW;
 import static ch.epfl.bbp.uima.testutils.UimaTests.TEST_SENTENCE;
 import static ch.epfl.bbp.uima.testutils.UimaTests.createAnnot;
 import static ch.epfl.bbp.uima.testutils.UimaTests.getTestCas;
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngine;
+import static org.apache.uima.fit.pipeline.SimplePipeline.runPipeline;
 import static org.apache.uima.fit.util.JCasUtil.select;
 import static org.junit.Assert.assertEquals;
 
@@ -12,13 +14,11 @@ import java.util.Collection;
 import org.apache.uima.analysis_engine.AnalysisEngine;
 import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.pipeline.SimplePipeline;
-import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 import org.junit.Test;
 
 import ch.epfl.bbp.uima.ae.BannerAnnotator;
 import ch.epfl.bbp.uima.ae.NaiveSentenceSplitterAnnotator;
-import ch.epfl.bbp.uima.testutils.UimaTests;
 import ch.epfl.bbp.uima.types.Protein;
 import ch.epfl.bbp.uima.typesystem.Prin;
 import de.julielab.jules.types.Sentence;
@@ -31,23 +31,25 @@ public class BannerAETest {
     @Test
     public void testText() throws Exception {
 
-        JCas jcas = UimaTests.getTestCas();
+        JCas jCas = getTestCas();
 
-        AnalysisEngine ss = AnalysisEngineFactory
-                .createEngine(NaiveSentenceSplitterAnnotator.class);
+        AnalysisEngine ss = createEngine(NaiveSentenceSplitterAnnotator.class);
+        AnalysisEngine banner = createEngine(BannerAnnotator.class);
 
-        AnalysisEngine banner = AnalysisEngineFactory
-                .createEngine(BannerAnnotator.class);
+        runPipeline(jCas, ss, banner);
 
-        SimplePipeline.runPipeline(jcas, ss, banner);
-
-        Collection<Protein> prots = JCasUtil.select(jcas, Protein.class);
+        Collection<Protein> prots = select(jCas, Protein.class);
         assertEquals(1, prots.size());
 
         Protein prot = prots.iterator().next();
         // TODO calbindin-D28k
         assertEquals("D28k", prot.getCoveredText());
         Prin.t(prots);
+
+        // pmid 8786446
+        jCas = getTestCas("Physiological and morphological characteristics of GABAergic nonpyramidal cells in frontal cortex of young rats identified immunohistochemically as containing somatostatin or vasoactive intestinal polypeptide (VIP) were studied in vitro by whole-cell recording and biocytin injection. We have found that most somatostatin- or VIP-containing neurons were different from two other types of GABAergic cells, the parvalbumin-containing fast-spiking cells and the late-spiking cells (neurogliaform cells). In response to injected currents, somatostatin- or VIP-containing nonpyramidal cells showed either bursts of a few spikes on a slow-depolarizing hump, burst-spiking nonpyramidal cells, or single spikes only on depolarization, regular-spiking nonpyramidal cells. Morphologically, both somatostatin- and VIP-containing cells had vertical axonal arbors terminating in symmetrical synapses that were immunoreactive for GABA in electron micrographs. Somatostatin cells included neurons with main ascending axons sending collaterals into layer I (Martinotti cells in deep layers). Some of the Martinotti cells in layer V also contained calbindin D 28k. VIP cells included neurons the main descending axons of which had more descending than ascending collaterals (bipolar cells and double bouquet cells). Two other morphological forms of the VIP cells were those with short descending axons with collaterals bearing multiple boutons on other cell bodies (small basket cells) or with short ascending main axons with collaterals forming arcades (arcade cells). Some of these neurons also contained calretinin. From these results, it appears that the GABAergic neurons controlling circuits in the neocortical layers may be characterized further based on whether they contain somatostatin or VIP.");
+        runPipeline(jCas, ss, banner);
+        Prin.t(select(jCas, Protein.class));
     }
 
     @Test
---
+++ modules/bluima_pdf/src/main/java/ch/epfl/bbp/uima/AbbreviationExpanderScript.java
@@ -0,0 +1,50 @@
+package ch.epfl.bbp.uima;
+
+import static ch.epfl.bbp.uima.BlueUima.PARAM_INPUT_FILE;
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+import static org.apache.uima.fit.factory.CollectionReaderFactory.createReaderDescription;
+import static org.apache.uima.fit.pipeline.SimplePipeline.runPipeline;
+import static org.apache.uima.fit.util.JCasUtil.select;
+
+import java.util.Collection;
+
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
+import org.apache.uima.jcas.JCas;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ch.epfl.bbp.uima.ae.AbbreviationsAnnotator;
+import ch.epfl.bbp.uima.cr.SingleFileReader;
+import ch.epfl.bbp.uima.pdf.cr.PdfCollectionAnnotator;
+import ch.epfl.bbp.uima.typesystem.To;
+import de.julielab.jules.types.Abbreviation;
+
+public class AbbreviationExpanderScript extends JCasAnnotator_ImplBase {
+    Logger LOG = LoggerFactory.getLogger(AbbreviationExpanderScript.class);
+
+    @Test
+    public void test() throws Exception {
+
+        runPipeline(
+                createReaderDescription(SingleFileReader.class,
+                        PARAM_INPUT_FILE,
+                        "src/test/resources/yuri/2019.full.pdf"), //
+                createEngineDescription(PdfCollectionAnnotator.class),//
+                createEngineDescription(AbbreviationsAnnotator.class,
+                        "retrain", true),//
+                createEngineDescription(AbbreviationExpanderScript.class)//
+        );
+
+    }
+
+    @Override
+    public void process(JCas jCas) throws AnalysisEngineProcessException {
+        Collection<Abbreviation> abrevs = select(jCas, Abbreviation.class);
+        for (Abbreviation abrev : abrevs) {
+            LOG.debug(To.string(abrev));
+        }
+    }
+
+}
